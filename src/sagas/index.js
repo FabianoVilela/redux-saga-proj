ca8c64aff75f3c42872d3ec9eb04642e1063b6b6
Fabiano Vilela <fabiano.vilela@gmail.com>
2018-07-19 20:00:08 -0300

    Injection of dependencies

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,13 @@
 import { all, takeLatest } from 'redux-saga/effects'
+import axios from 'axios'
+
 import getIP from './ip'
 import getUA from './ua'
 
 function *index() {
   yield all([
-          takeLatest('LOAD_DATA_REQUEST', getIP),
-          takeLatest('LOAD_DATA_UA_REQUEST', getUA)
+          takeLatest('LOAD_DATA_REQUEST', getIP, axios),
+          takeLatest('LOAD_DATA_UA_REQUEST', getUA, axios)
         ])
 }
 
--- src/sagas/ua.js
+++ src/sagas/ua.js
@@ -1,9 +1,8 @@
 import { put } from 'redux-saga/effects'
-import axios from 'axios'
 
 import { loadDataUASuccess } from './../actions'
 
-function *getUA() {
+function *getUA(axios) {
   const res = yield axios.get('http://httpbin.org/user-agent')
   yield put(loadDataUASuccess(res.data['user-agent']))
 }
